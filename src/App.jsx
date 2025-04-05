19a5e00ce2ef36ea279da5a7e66cec9f20f131a0
Raunak Pal <raunakpal51441@gmail.com>
2025-04-05 11:17:56 +0530

    Social Media App

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import styles from "./App.module.css";
+import CreatePost from "./components/CreatePost";
+import Posts from "./components/Posts";
+import PostList from "./components/PostList";
+import PostListProvider from "./store/post-list-store";
+
+function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
+
+  return (
+    <PostListProvider>
+      <div className={styles.appContainer}>
+        <Sidebar
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
+        ></Sidebar>
+        <div className={styles.rightContainer}>
+          <Header></Header>
+          {selectedTab === "Home" ? (
+            <PostList></PostList>
+          ) : (
+            <CreatePost></CreatePost>
+          )}
+          <Footer></Footer>
+        </div>
+      </div>
+    </PostListProvider>
+  );
+}
+
+export default App;
---
+++ src/components/PostList.jsx
@@ -0,0 +1,11 @@
+import Posts from "./Posts"
+const PostList = () => {
+    return (<>
+        <Posts></Posts>
+        <Posts></Posts>
+        <Posts></Posts>
+    </>
+    )
+}
+
+export default PostList;
